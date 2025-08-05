776e555474e8425e66ef002d006e08e0959cc8f8
Stéphane Morel <morelstephane4026@neuf.fr>
2025-08-05 15:17:58 +0200

    revert 7630222

--- lvl1/seeds.gd
+++ lvl1/seeds.gd
@@ -1,16 +1,9 @@
+
 extends Node2D
 
-func _ready():
-	# 🔁 Récupère le GameState à la racine du projet
-	var game_state = get_node("/root/GameState")
+@export var seed_value: int = 1
 
-	# 🔢 Compte les graines (enfants directs de ce noeud)
-	var nombre_graines = get_child_count()
-	game_state.reset_seed_tracking(nombre_graines)
-
-	# 🔍 Cherche le totem directement dans la scène lvl1
-	var totem = get_node_or_null("/root/GameState/lvl1/Totem")
-
-	# ✅ Si trouvé, connecte le signal
-	if totem.has_method("on_all_seeds_collected"):
-		game_state.connect("all_seeds_collected", Callable(totem, "on_all_seeds_collected"))
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") and body.has_method("collect_seed"):
+		body.collect_seed(1) 
+		queue_free()
