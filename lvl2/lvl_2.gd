02695fa2fbcd1628f3b08da3870723199c5db731
Stéphane Morel <morelstephane4026@neuf.fr>
2025-08-08 00:45:45 +0200

    ambiance lvl2 (éclairage scene, torche, musique)

--- lvl2/lvl_2.gd
+++ lvl2/lvl_2.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	#print_tree_pretty()
+	$Sound/lvl2.play()
+	# Répetition du paralaxe
 	var paralaxe_fond = $ParalaxeFond
 	if paralaxe_fond:
 		var pb = paralaxe_fond.get_node("ParallaxBackground")
@@ -9,4 +10,11 @@ func _ready():
 			var layer = pb.get_node("ParallaxLayer_fond")
 			if layer:
 				layer.motion_scale = Vector2(0.5, 0.5)
-			
+	
+	await get_tree().process_frame  
+	
+	# Assombrissement de Moko
+	var gs = get_node_or_null("/root/GameState")
+	if gs and gs.player:
+		var moko = gs.player
+		moko.get_node("Sprite").modulate = Color(0.4, 0.4, 0.4)  
