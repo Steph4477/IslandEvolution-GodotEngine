16496e29dfd1b95e33b1ee69f37a5d192c8be38e
Stéphane Morel <morelstephane4026@neuf.fr>
2025-08-07 11:14:19 +0200

    retouche animations arbres

--- Loot/cocotier_loot.gd
+++ Loot/cocotier_loot.gd
@@ -3,6 +3,10 @@ extends Node2D
 @export var coco_value: int = 1
 @export var activate_shooting: bool = false
 
+
+#func _ready() -> void:
+	#$AnimationPlayer.play("wind")
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_method("collect_coco"):
 		body.collect_coco(3, true)  
