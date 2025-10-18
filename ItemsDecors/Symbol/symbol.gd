311bc8707c7e7e4b8a62431f62e8f2116b08ce12
Stéphane Morel <morelstephane4026@neuf.fr>
2025-10-18 08:35:29 +0200

    code scène symboles réutilisable pour pouvoir changer le sprite -> mise en place du lion et de la chouette dans la scène principale

--- ItemsDecors/Symbol/symbol.gd
+++ ItemsDecors/Symbol/symbol.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
-func _ready() -> void:
-	$TurnOn.visible = false
-	$TurnOff.visible = true
-	
-func _on_area_2d_body_entered(body) -> void:
-	if body.is_in_group("Player"):
-		$TurnOn.visible = true
-		$TurnOff.visible = false
+@export var texture_off: Texture2D
+@export var texture_on: Texture2D
+
+@onready var area = $Area2D
+@onready var turn_on = $TurnOn
+@onready var turn_off = $TurnOff
+
+func _ready():
+	# Applique les textures exportées à l'instance
+	if texture_off:
+		turn_off.texture = texture_off
+	if texture_on:
+		turn_on.texture = texture_on
+
+	# État initial
+	turn_on.visible = false
+	turn_off.visible = true
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player") or body.name == "Player":
+		turn_on.visible = true
+		turn_off.visible = false
