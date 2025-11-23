e5916c00cc4f3c13c873f936ad0e4f801a548642
Stéphane Morel <morelstephane4026@neuf.fr>
2025-11-23 14:10:20 +0100

    feat(Tree_perch):Ajout des nouvelles textures de sprites

--- Levels/Lvl3/Environement/Tree_perch/tree_perch.gd
+++ Levels/Lvl3/Environement/Tree_perch/tree_perch.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_door_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		$AnimationPlayer.play("fade")
+		await get_tree().create_timer(0.8).timeout
 		$Full.visible = false
