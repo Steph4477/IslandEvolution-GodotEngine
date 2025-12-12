78a5e76e77433c5832113df2123d9f29ecd90327
Stéphane Morel <morelstephane4026@neuf.fr>
2025-12-13 00:10:30 +0100

    feat(bee_hive, honey, tree_perch): Ajout de la ruche dans l'arbre avec loot de potion de miel et spawn d'abeilles

--- Loot/BeeHive/bee_hive.gd
+++ Loot/BeeHive/bee_hive.gd
@@ -1,44 +1,23 @@
 extends Node2D
 
 @export var bee_scene = preload("res://Enemies/Bee/bee.tscn")
-@export var spawn_radius = 74.0
-@export var one_shot = true
+@export var honey_loot_scene = preload("res://Loot/Honey/honey.tscn")
 
-@onready var sprite = $Sprite2D
-
-var has_spawned = false
-
+@onready var spawn_timer = $SpawnTimer
 
 func _on_area_2d_body_entered(body):
-	# On ne réagit qu'au joueur
-	if not body.is_in_group("Player"):
-		return
+	if body.is_in_group("Player"):
+		spawn_timer.start()
 
-	# Si déjà déclenchée et one_shot, on ne refait rien
-	if one_shot and has_spawned:
-		return
-
-	_spawn_bee()
-
-	# La ruche sert qu'une fois -> on la supprime
-	if one_shot:
-		queue_free()
-
-
-func _spawn_bee():
-	has_spawned = true
-
-	if not bee_scene:
-		return
-
-	# Instanciation d'une seule abeille
+func _on_spawn_timer_timeout():
 	var bee = bee_scene.instantiate()
 	get_parent().add_child(bee)
-	
-	# --- Z-INDEX AU POP ---
+	bee.global_position = global_position
 	bee.z_index = 20
-	
-	# Placement de l’abeille autour de la ruche
-	var angle = randf() * TAU              # direction aléatoire
-	var offset = Vector2(cos(angle), sin(angle)) * spawn_radius
-	bee.global_position = global_position + offset
+
+	var loot = honey_loot_scene.instantiate()
+	get_parent().add_child(loot)
+	loot.global_position = global_position
+	loot.z_index = 20
+
+	queue_free()
---
+++ Loot/Honey/honey.gd
@@ -0,0 +1,31 @@
+extends RigidBody2D
+
+@export var banane_value = 5
+@export var heal_amount = 500
+
+@onready var follower = $Path2D/PathFollow2D
+@onready var collision = $Path2D/PathFollow2D/PickupArea/CollisionShape2D
+@onready var anim = $AnimationPlayer
+@onready var collision_honey = $CollisionShape2D
+
+var game_state
+var can_pickup = false
+
+func _ready():
+	collision.disabled = true
+	can_pickup = false
+	
+	anim.play("appear")
+	await anim.animation_finished
+	
+	can_pickup = true
+	collision.disabled = false
+	
+	game_state = get_node_or_null("/root/GameState")
+	if game_state:
+		game_state.heal_amount = heal_amount
+
+func _on_pickup_area_body_entered(body):
+	if body.is_in_group("Player") and body.has_method("collect_banane"):
+		body.collect_banane(banane_value)
+		queue_free()
