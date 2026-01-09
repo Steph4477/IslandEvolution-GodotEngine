0d7d40ea619ba150fcd1d6a9b5aaedc6c043261b
Stéphane Morel <morelstephane4026@neuf.fr>
2026-01-09 12:42:48 +0100

    feature(lvl3): exit => lvl4

---
+++ Levels/Lvl3/exit.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var next_scene_path = "res://Levels/Lvl4/lvl_4.tscn"
+
+func _on_body_entered(_body):
+	change_scene()
+
+func change_scene():
+	var gs = get_node_or_null("/root/GameState")
+	gs.load_level(next_scene_path)
