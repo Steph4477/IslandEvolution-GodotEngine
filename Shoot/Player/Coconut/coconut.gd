9426b0efcbb780419b12a4d06f4fb554871e27e6
Steph4477 <morelstephane4026@neuf.fr>
2026-04-09 17:09:38 +0200

    feat(coconut): ajout de l’impact sur collision (ennemis et plateformes)
    - déclenchement de l’effet fire_impact à la collision
    - gestion des dégâts sur les ennemis
    - impact appliqué aussi sur les plateformes

--- Shoot/Player/Coconut/coconut.gd
+++ Shoot/Player/Coconut/coconut.gd
@@ -1,31 +1,38 @@
 extends RigidBody2D
 
-@export var speed := 1000
-@export var life_time := 3.0
-@export var damage := 100  # Dégâts infligés par la banane
+@export var speed = 1000
+@export var life_time = 3.0
+@export var damage = 100
 
-var direction := 1  # 1 = droite, -1 = gauche
+var direction = 1
 
-func start(pos: Vector2, dir: int) -> void:
-	position = pos
+const FIRE_IMPACT_SCENE = preload("res://Effects/Fire/Fire_impact/fire_impact.tscn")
+
+func start(pos, dir):
 	direction = dir
+	global_position = pos
 	linear_velocity = Vector2(speed * direction, 0)
-	$Area2D/CollisionShape2D.disabled = false 
-	call_deferred("_self_destruct") 
-	
-func _self_destruct() -> void:
+
+	$Area2D/CollisionShape2D.disabled = true
+
+	call_deferred("_enable_hitbox")
+	call_deferred("_self_destruct")
+
+func _enable_hitbox():
+	await get_tree().process_frame
+	$Area2D/CollisionShape2D.disabled = false
+
+func _self_destruct():
 	await get_tree().create_timer(life_time).timeout
 	queue_free()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Enemies"):
-		if body.has_method("on_hit"):
-			body.on_hit(damage)
-		if body.has_method("stone"): #assomement
-			body.stone()
-	else:
-		print("Cible NON dans le groupe Enemies")
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Enemies") and body.has_method("on_hit"):
+		body.on_hit(damage)
 
-	$Area2D/CollisionShape2D.set_deferred("disabled", true)
-	$Sprite.hide()
+	var impact = FIRE_IMPACT_SCENE.instantiate()
+	get_tree().current_scene.add_child(impact)
+	impact.global_position = global_position + Vector2(direction * 8, -6)
+	impact.play_impact()
+
 	queue_free()
