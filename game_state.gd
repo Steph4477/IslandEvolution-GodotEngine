c128e99f8f41f3af2d7e821b2d9c2c861ad73129
Steph4477 <morelstephane4026@neuf.fr>
2026-04-14 09:02:26 +0200

    feat(lvl): ajout de la quête de craft du skill feu dans les niveaux
    - doublement du village dans le lvl_3
    
    - ajout de l'autel de craft
    
    - ajustement du discours de Toucan dans chronoZone

--- game_state.gd
+++ game_state.gd
@@ -109,9 +109,9 @@ func _ready():
 	reset_session_dialogues()
 
 	await get_tree().process_frame
-	await load_level("res://Levels/Test/test_scene.tscn")
+	#await load_level("res://Levels/Test/test_scene.tscn")
 	#await load_level("res://Levels/Lvl2/lvl_2.tscn")
-	#await load_level("res://Levels/Lvl3/lvl_3.tscn")
+	await load_level("res://Levels/Lvl3/lvl_3.tscn")
 	#await load_level("res://Levels/Lvl4/lvl_4.tscn")
 
 func _process(_delta):
