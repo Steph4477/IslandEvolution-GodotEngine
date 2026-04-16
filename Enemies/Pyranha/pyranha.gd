43ffcb9342e8339fc8e8c0742dbdd07b8e107fc6
Steph4477 <morelstephane4026@neuf.fr>
2026-04-17 00:40:12 +0200

    feat(pyranha): ajout attaque simple de face
    - testé et validé en scène

--- Enemies/Pyranha/pyranha.gd
+++ Enemies/Pyranha/pyranha.gd
@@ -17,21 +17,54 @@ func _physics_process(delta):
 	if is_dead:
 		return
 
-	# refresh player
 	if player == null or not is_instance_valid(player):
 		refresh_player()
 
-	# update target
 	target_mod.update()
 
-	# décision simple
+	if in_melee: 
+		play_attack()
+		return
+
 	if target != null:
 		chase_target()
 		play_chase()
 	else:
 		patrol_mod.process(delta)
-
 		play_swim()
+
+func attack():
+	if is_dead:
+		return
+
+	play_attack()
+
+	if player != null and is_instance_valid(player):
+		player.damage_mod.on_hit(damage)
 
 func _on_patrol_timer_timeout():
 	patrol_mod.on_timeout()
+
+func _on_attack_zone_body_entered(body):
+	if is_dead:
+		return
+
+	if body.is_in_group("Player"):
+		in_melee = true
+		attack()
+		attack_timer.start()
+
+func _on_attack_zone_body_exited(body):
+	if body.is_in_group("Player"):
+		in_melee = false
+		attack_timer.stop()
+
+func _on_attack_timer_timeout():
+	if is_dead:
+		return
+
+	if not in_melee:
+		return
+
+	attack()
+	attack_timer.start()
