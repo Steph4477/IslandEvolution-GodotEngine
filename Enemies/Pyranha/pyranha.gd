f0052a18338bec6b5dfbffa1d540fa3a19afec7a
Steph4477 <morelstephane4026@neuf.fr>
2026-04-17 09:17:16 +0200

    feat(piranha): verrouillage target + contrôle du rythme d'attaque
    - lock de la target après acquisition
    
    - suppression du spam d'attaque via is_attacking
    
    - ajout de attack_interval exporté
    
    - synchronisation attack_timer.wait_time avec attack_interval
    
    - comportement melee stabilisé (attaque déclenchée uniquement par timer)
    
    - instancié et testé dans la scène

--- Enemies/Pyranha/pyranha.gd
+++ Enemies/Pyranha/pyranha.gd
@@ -1,11 +1,15 @@
 extends EnemySwimBase
 class_name Pyranha
 
+@export var attack_interval = 1.0
+
 var target_mod
 var patrol_mod
 
 func _ready():
 	super._ready()
+	if attack_timer:
+		attack_timer.wait_time = attack_interval
 
 	target_mod = EnemyModTarget.new()
 	target_mod.setup(self)
@@ -20,10 +24,15 @@ func _physics_process(delta):
 	if player == null or not is_instance_valid(player):
 		refresh_player()
 
-	target_mod.update()
+	if target == null:
+		target_mod.update()
 
-	if in_melee: 
-		play_attack()
+	if in_melee:
+		velocity = Vector2.ZERO
+
+		if not is_attacking:
+			play_attack()
+
 		return
 
 	if target != null:
@@ -34,13 +43,20 @@ func _physics_process(delta):
 		play_swim()
 
 func attack():
-	if is_dead:
+	if not can_attack_player():
 		return
+
+	is_attacking = true
+	velocity = Vector2.ZERO
 
 	play_attack()
 
 	if player != null and is_instance_valid(player):
 		player.damage_mod.on_hit(damage)
+
+	await get_tree().create_timer(attack_interval).timeout
+
+	is_attacking = false
 
 func _on_patrol_timer_timeout():
 	patrol_mod.on_timeout()
@@ -51,13 +67,19 @@ func _on_attack_zone_body_entered(body):
 
 	if body.is_in_group("Player"):
 		in_melee = true
-		attack()
-		attack_timer.start()
+
+		if not is_attacking:
+			attack()
+
+		if attack_timer:
+			attack_timer.start()
 
 func _on_attack_zone_body_exited(body):
 	if body.is_in_group("Player"):
 		in_melee = false
-		attack_timer.stop()
+
+		if attack_timer:
+			attack_timer.stop()
 
 func _on_attack_timer_timeout():
 	if is_dead:
@@ -66,5 +88,7 @@ func _on_attack_timer_timeout():
 	if not in_melee:
 		return
 
+	if is_attacking:
+		return
+
 	attack()
-	attack_timer.start()
