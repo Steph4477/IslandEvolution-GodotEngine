a8010f131c6618e35c0d738e158e1ac0aba19228
Steph4477 <morelstephane4026@neuf.fr>
2026-04-23 17:41:27 +0200

    feat(skill_air): interaction idole air avec anim et input E
    - ajout zone interact avec détection player_in_zone
    
    - animation appear_air en boucle à l'entrée
    
    - interaction via touche interact (E)
    
    - intégration player.popups_mod pour feedback UI
    
    - désactivation monitoring après collecte
    
    - synchronisation avec GameState (idole_collected + checklist HUD)

--- Items/Idole_Air/idole_air_item.gd
+++ Items/Idole_Air/idole_air_item.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
 var collected = false
+var player_in_zone = false
+var player = null
+var is_collecting = false
+
 @onready var anim = $AnimationPlayer
 @onready var fullSprite = $FullSprite
 @onready var emptySprite = $EmptySprite
-@onready var collision = $CollisionShape2D
 
 func _ready():
 	var gs = get_node("/root/GameState")
+
 	fullSprite.visible = true
 	emptySprite.visible = false
 	
@@ -15,26 +19,66 @@ func _ready():
 		fullSprite.visible = false
 		emptySprite.visible = true
 		collected = true
-		collision.set_deferred("disabled", true)
 		set_deferred("monitoring", false)
 
 func _on_body_entered(body):
+	if body.is_in_group("Player") and not collected and not is_collecting:
+		fullSprite.visible = true
+		emptySprite.visible = false
+		player_in_zone = true
+		player = body
+		anim.play("appear_air")
+
+		if player.popups_mod:
+			player.popups_mod.show_info("Appuie sur 'E' pour récupérer l'idole de l'air")
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		player_in_zone = false
+		player = null
+
+		if not collected and not is_collecting:
+			anim.stop()
+		
+		anim.play("RESET")
+		
+
+func _process(_delta):
+	if not player_in_zone:
+		return
+
+	if is_collecting:
+		return
+
+	if Input.is_action_just_pressed("interact"):
+		collect()
+
+func collect():
 	if collected:
 		return
 
-	if body.is_in_group("Player"):
-		collected = true
-		collision.set_deferred("disabled", true)
-		set_deferred("monitoring", false)
+	if is_collecting:
+		return
 
-		anim.play("appear_air")
-		await anim.animation_finished
+	is_collecting = true
+	player_in_zone = false
 
-		fullSprite.visible = false
-		emptySprite.visible = true
+	anim.stop()
 
-		body.collect_items.collect_idole()
+	player.animation_mod.set_interact(true)
+	await player.anim.animation_finished
+	player.animation_mod.set_interact(false)
 
-		var gs = get_node("/root/GameState")
-		if gs.hud:
-			gs.hud.update_air_craft_checklist()
+	$ParticlesLoot.visible = false
+	fullSprite.visible = false
+	emptySprite.visible = true
+
+	collected = true
+	player.collect_items.collect_idole()
+
+	var gs = get_node("/root/GameState")
+	if gs.hud:
+		gs.hud.update_air_craft_checklist()
+
+	set_deferred("monitoring", false)
+	is_collecting = false
