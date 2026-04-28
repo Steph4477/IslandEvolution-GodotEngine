4c4cf2507e0cb3f11f0f54fa7a0d1721ffd97f99
Steph4477 <morelstephane4026@neuf.fr>
2026-04-28 17:58:12 +0200

    feat(lvl_2a): cinématique d'intro toucan
    - ajout des sprites et animations
    
    - entrée du toucan dans l'antichambre
    
    - intégration du dialogue sous-titré
    
    - ajout du loot de récompense

---
+++ Levels/Lvl2/lvl_2a/CinematicToucan/cinematic_toucan.gd
@@ -0,0 +1,107 @@
+extends Node2D
+
+@onready var loot_scene = preload("res://Items/Fire_Recipe/fire_recipe.tscn")
+@onready var anim = $AnimationPlayer
+@onready var toucan = $Toucan
+@onready var box  = $Box
+@onready var text = $Box/MarginContainer/Text
+
+# Réglages via l’inspecteur
+@export var lines = [
+	"Pas mal pour un singe !",
+	"Tu as trouvé bois et pierre",
+	"Je commence à être impressionné…",
+	"Tiens, prends ça !",
+	"À toi de maîtriser le feu maintenant."
+]
+
+@export var auto_start := true           # Démarrer au _ready
+@export var chars_per_sec := 30          # Vitesse d’écriture
+@export var pause_between_lines := 1.0   # Pause entre les lignes (s)
+@export var hide_when_done := true       # Cacher le bandeau à la fin
+
+var player
+var gs
+
+# --- Sous-titre ---
+var line_index := -1
+var full_line := ""
+var shown_chars := 0
+var writing := false
+var accum := 0.0
+var pause_left := 0.0
+
+func _ready():
+	gs = get_node("/root/GameState")
+	player = gs.player
+	
+	start_cinematic()
+	
+	await get_tree().create_timer(4).timeout
+	start()
+	await get_tree().create_timer(4.4).timeout
+	loot_craft()  
+
+func start_cinematic():
+	player.can_move = false
+	anim.play("intro")
+
+func start(new_lines = null):
+	# Lance le dialogue. Si new_lines est fourni, il remplace lines.
+	if new_lines != null:
+		lines = new_lines.duplicate()
+	line_index = 0
+	visible = true
+	box.visible = true
+	set_process(true)
+	_start_line()
+
+func _start_line():
+	if line_index >= lines.size():
+		_end_dialogue()
+		return
+	full_line = str(lines[line_index])
+	shown_chars = 0
+	accum = 0.0
+	writing = true
+	pause_left = 0.0     
+	_render()
+
+func _process(delta):
+	if not box.visible:
+		return
+
+	if writing:
+		accum += delta
+		var step = int(accum * chars_per_sec)
+		if step > 0:
+			accum -= float(step) / float(chars_per_sec)
+			shown_chars += step
+			if shown_chars >= full_line.length():
+				shown_chars = full_line.length()
+				writing = false
+				pause_left = pause_between_lines
+			_render()
+	elif pause_left > 0.0:
+		pause_left -= delta
+		if pause_left <= 0.0:
+			line_index += 1
+			_start_line()
+
+func _render():
+	text.text = full_line.substr(0, shown_chars)
+	player.can_move = true
+
+func _end_dialogue():
+	if hide_when_done:
+		box.visible = false
+		visible = false
+	set_process(false)
+
+func loot_craft():
+	var loot = preload("res://Items/Fire_Recipe/fire_recipe.tscn").instantiate()
+	var marker = $LootSpawn
+	get_tree().current_scene.add_child(loot)
+	
+	# Position sur le marker
+	loot.global_position = marker.global_position
--- Levels/Lvl2/lvl_2a/lvl_2a.gd
+++ Levels/Lvl2/lvl_2a/lvl_2a.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+@onready var marker = $IntroToucanSpawn
+
 func _ready():
 	$Node2D/Sound/lvl2.play()
 	await get_tree().process_frame
 
+	start_cinematic()
+	
 	var gs = get_node("/root/GameState")
 	
 	# Caméra + assombrissement Moko
@@ -14,3 +18,10 @@ func _ready():
 
 		var moko = gs.player
 		moko.get_node("Node2D/Sprite").modulate = Color(0.985, 0.791, 0.76, 1.0)
+
+func start_cinematic():
+	var cine = preload("res://Levels/Lvl2/lvl_2a/CinematicToucan/cinematic_toucan.tscn").instantiate()
+	get_tree().current_scene.add_child(cine)
+	
+	# Position sur le marker
+	cine.global_position = marker.global_position
