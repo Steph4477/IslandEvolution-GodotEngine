8a650cc7fbbac199479742eeb99851b038f33265
Steph4477 <morelstephane4026@neuf.fr>
2026-04-30 18:19:40 +0200

    feat(pile) insertion pile de cocos et bananes dans l'antichambre avant boss
    - logique alignée sur bag_loot

---
+++ Loot/Coconut/Pile/pile.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var coco_value = 1
+@export var banane_value = 1
+@export var heal_amount = 500
+@export var activate_shooting = false
+
+var game_state
+
+func _ready():
+	game_state = get_node_or_null("/root/GameState")
+	game_state.heal_amount = heal_amount # Initialise le montant de pv par potion
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.collect_items.collect_coco(20, true)
+		queue_free()
+	if body.is_in_group("Player"):
+		body.collect_items.collect_banane(6)
+		queue_free()  
