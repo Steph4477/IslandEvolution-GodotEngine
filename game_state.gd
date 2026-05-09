ea27a84b1888c10bdb4651ee0c10ae041d9904ce
Steph4477 <morelstephane4026@neuf.fr>
2026-05-09 18:41:20 +0200

    feat(lvl_3): ajout de l'entrée de l'arène boss
    - allongement du sprite village2 pour gagner en espace
    
    - ajout du couloir d'entrée de l'arène en deux parties
    
    - utilisation de z_index différents pour l'effet d'entrée de Moko dans le couloir
    
    - instancié et testé en scene

--- game_state.gd
+++ game_state.gd
@@ -120,11 +120,11 @@ func _ready():
 
 	await get_tree().process_frame
 	#await load_level("res://Levels/Test/test_scene.tscn")
-	await load_level("res://Levels/Lvl2/lvl_2.tscn")
+	#await load_level("res://Levels/Lvl2/lvl_2.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2a/lvl_2a.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2b/lvl_2b.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2c/lvl_2c.tscn")
-	#await load_level("res://Levels/Lvl3/lvl_3.tscn")
+	await load_level("res://Levels/Lvl3/lvl_3.tscn")
 	#await load_level("res://Levels/Lvl4/lvl_4.tscn")
 
 func _process(_delta):
