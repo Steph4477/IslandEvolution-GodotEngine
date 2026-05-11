7ffeb71c03ce5c456f2ff547d98ecb878a953580
Steph4477 <morelstephane4026@neuf.fr>
2026-05-11 11:52:01 +0200

    feat(lvl_3b): ajout de la cinématique d'introduction de l'arène cannibale
    - ajout d'un faux Moko pour la séquence cinématique
    
    - ajout d'une caméra cinématique avec dézoom progressif
    
    - masquage temporaire du vrai Moko et du Toucan pendant l'intro
    
    - activation du vrai joueur à la fin de la cinématique
    
    - switch automatique entre caméra cinématique et caméra gameplay
    
    - ajout du repositionnement du joueur au SpawnPoint
    
    - ajout du zoom gameplay de fin d'introduction
    
    - ajustement de l'échelle du vrai Moko après la transition
    
    - modification et ajouts d'assets de l'arene pour gagner en profondeur

--- Levels/Lvl3/Lvl_3b/lvl_3b.gd
+++ Levels/Lvl3/Lvl_3b/lvl_3b.gd
@@ -1,23 +1,67 @@
 extends Node2D
 
-@onready var player_spawn = $SpawnPoint
-
-var cam
+@onready var player_spawn = $World/Arena/SpawnPoint
+@onready var toucan = $World/Toucan
+@onready var fake_moko = $World/IntroCinematic/FakeMoko
+@onready var fake_moko_anim = $World/IntroCinematic/CameraCinematic/AnimationPlayer
+@onready var scene_camera = $World/IntroCinematic/Camera2D
 
 func _ready():
-	await get_tree().process_frame
-
 	var gs = get_node("/root/GameState")
 
-	if gs.player == null:
-		var player = gs.player_scene.instantiate()
-		add_child(player)
-		player.global_position = player_spawn.global_position
-		gs.player = player
-		gs.emit_signal("player_updated", player)
-	
-	# --- Limite caméra & assombrissement ---
-	cam = gs.player.get_node("Camera2D")
-	cam.limit_right = 1900
-	cam.limit_bottom = 1000
-	
+	await get_tree().process_frame
+
+	# Instancie, rends invisible, diminue sa taille et zoom sur le vrai Moko au lancement
+	var player = gs.player
+	var player_camera = player.get_node("Camera2D")
+
+	player.visible = false
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+	player.global_position = player_spawn.global_position
+
+	player.scale = Vector2(0.8, 0.8)
+
+	player_camera.enabled = false
+
+	# Rends invisible le Toucan au lancement (son déplacement dépend si player nul)
+	toucan.visible = false
+	toucan.process_mode = Node.PROCESS_MODE_DISABLED
+
+	scene_camera.enabled = true
+	scene_camera.make_current()
+
+	player_camera.zoom = Vector2(1.5, 1.5)
+
+	# Rends visible le faux Moko (acteur de la cinematique)
+	fake_moko.visible = true
+	fake_moko_anim.play("intro")
+
+
+# --- Fin de cinématique ---
+func end_intro():
+	var gs = get_node("/root/GameState")
+	var player = gs.player
+	var player_camera = player.get_node("Camera2D")
+
+	# Supprime l'acteur de la cinématique
+	fake_moko.queue_free()
+
+	player.visible = true
+	player.process_mode = Node.PROCESS_MODE_INHERIT
+	player.global_position = player_spawn.global_position
+
+	toucan.visible = true
+	toucan.process_mode = Node.PROCESS_MODE_INHERIT
+
+	await get_tree().process_frame
+
+	scene_camera.enabled = false
+
+	player_camera.enabled = true
+	player_camera.make_current()
+	player_camera.limit_right = 1600
+	player_camera.limit_bottom = 1000
+
+# --- Signal de fin d'animation
+func _on_animation_player_animation_finished(anim_name):
+	end_intro()
