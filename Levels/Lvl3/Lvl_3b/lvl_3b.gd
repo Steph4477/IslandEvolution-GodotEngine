693e46fd0fa2fad24bd522363f6e4b8874d1d1c9
Steph4477 <morelstephane4026@neuf.fr>
2026-05-12 16:57:58 +0200

    feat(lvl_3b): ajout d'éléments dans la cinématique d'introduction
    - les os en arrière-plan montent avec un effet de tremblement
    
    - ajout d'une barrière de dents pour renforcer l'effet d'emprisonnement
    
    - réduction de la taille du public cannibale
    
    - ajout d'un fondu en fin de cinématique
    
    - scène instanciée et testée en jeu

--- Levels/Lvl3/Lvl_3b/lvl_3b.gd
+++ Levels/Lvl3/Lvl_3b/lvl_3b.gd
@@ -11,39 +11,38 @@ func _ready():
 
 	await get_tree().process_frame
 
-	# Instancie, rends invisible, diminue sa taille et zoom sur le vrai Moko au lancement
 	var player = gs.player
 	var player_camera = player.get_node("Camera2D")
 
 	player.visible = false
 	player.process_mode = Node.PROCESS_MODE_DISABLED
 	player.global_position = player_spawn.global_position
-
 	player.scale = Vector2(0.8, 0.8)
 
 	player_camera.enabled = false
 
-	# Rends invisible le Toucan au lancement (son déplacement dépend si player nul)
 	toucan.visible = false
 	toucan.process_mode = Node.PROCESS_MODE_DISABLED
 
 	scene_camera.enabled = true
 	scene_camera.make_current()
 
-	player_camera.zoom = Vector2(1.5, 1.5)
+	player_camera.zoom = Vector2(0.75, 0.75)
 
-	# Rends visible le faux Moko (acteur de la cinematique)
 	fake_moko.visible = true
 	fake_moko_anim.play("intro")
 
 
-# --- Fin de cinématique ---
 func end_intro():
 	var gs = get_node("/root/GameState")
 	var player = gs.player
 	var player_camera = player.get_node("Camera2D")
+	var fade = gs.fade
 
-	# Supprime l'acteur de la cinématique
+	fade.fade_out()
+
+	await get_tree().create_timer(0.5).timeout
+
 	fake_moko.queue_free()
 
 	player.visible = true
@@ -59,9 +58,11 @@ func end_intro():
 
 	player_camera.enabled = true
 	player_camera.make_current()
-	player_camera.limit_right = 1600
-	player_camera.limit_bottom = 1000
+	player_camera.limit_right = 1900
+	player_camera.limit_bottom = 1750
 
-# --- Signal de fin d'animation
-func _on_animation_player_animation_finished(anim_name):
+	fade.fade_in()
+
+
+func _on_animation_player_animation_finished(_anim_name):
 	end_intro()
