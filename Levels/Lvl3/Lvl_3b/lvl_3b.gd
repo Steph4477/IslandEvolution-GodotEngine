f470ec32e445e144499a98248f0f6c1a71b4444e
Steph4477 <morelstephane4026@neuf.fr>
2026-05-13 16:46:20 +0200

    feat(lvl_3b): ajout de la vague de serpents et du flow d'arène
    - ajout du système de vague serpent dans l'arène du boss cannibale
    
    - ajout du suivi automatique des ennemis via groupe snake
    
    - ajout du spawn des serpents avec z_index et flip visuel dédié
    
    - ajout du flow public -> loot -> vague serpent
    
    - ajout du zoom caméra après l'introduction
    
    - conservation du snake.gd sans modification

--- Levels/Lvl3/Lvl_3b/lvl_3b.gd
+++ Levels/Lvl3/Lvl_3b/lvl_3b.gd
@@ -1,34 +1,24 @@
 extends Node2D
 
-@onready var player_spawn = $World/Arena/SpawnPoint
+@onready var player_spawn = $World/Arena/Visual/SpawnPoint
 @onready var toucan = $World/Toucan
 @onready var fake_moko = $World/IntroCinematic/FakeMoko
 @onready var anim = $World/IntroCinematic/CameraCinematic/AnimationPlayer
 @onready var scene_camera = $World/IntroCinematic/Camera2D
 @onready var tribune_thrower = $World/Arena/TribuneThrower
 
+@onready var snake_spawn_1 = $World/Arena/WavesEnemies/WaveSnake/SnakeSpawn1
+@onready var snake_spawn_2 = $World/Arena/WavesEnemies/WaveSnake/SnakeSpawn2
+@onready var snake_spawn_3 = $World/Arena/WavesEnemies/WaveSnake/SnakeSpawn3
+
 var intro_finished = false
+var enemies_alive = 0
+var snake_wave_finished = false
+
+var snake_scene = preload("res://Enemies/Snake/snake.tscn")
 
 func _ready():
-	var gs = get_node("/root/GameState")
-
-	await get_tree().process_frame
-
-	var player = gs.player
-
-	player.visible = false
-	player.process_mode = Node.PROCESS_MODE_DISABLED
-	player.global_position = player_spawn.global_position
-	player.scale = Vector2(0.8, 0.8)
-
-	toucan.visible = false
-	toucan.process_mode = Node.PROCESS_MODE_DISABLED
-
-	scene_camera.enabled = true
-	scene_camera.make_current()
-
-	fake_moko.visible = true
-	anim.play("intro")
+	start_intro()
 
 	# --- vague de loots intro ---
 	await get_tree().create_timer(3.5).timeout
@@ -49,8 +39,23 @@ func _ready():
 	# --- Spawn vague de serpents ---
 	await get_tree().create_timer(2.5).timeout
 	start_public_anim()
-	anim.play("after_intro")
+	anim.play("zoom_out_camera")
+	start_snake_wave()
 
+func _process(_delta):
+
+	# --- Verification des serpents encore vivants ---
+	if enemies_alive > 0 and snake_wave_finished == false:
+
+		update_wave_state()
+
+		if enemies_alive <= 0:
+			snake_wave_finished = true
+			end_snake_wave()
+
+# ============================================================================
+#                          MAITRISE DU PUBLIQUE
+# ============================================================================
 func start_public_anim():
 	get_tree().call_group("public_cannibal", "start_public_anim")
 
@@ -58,6 +63,29 @@ func start_public_anim():
 func stop_public_anim():
 	get_tree().call_group("public_cannibal", "stop_public_anim")
 
+# ============================================================================
+#                           CINEMATIQUE D'INTRODUCTION
+# ============================================================================
+func start_intro():
+	var gs = get_node("/root/GameState")
+
+	await get_tree().process_frame
+
+	var player = gs.player
+
+	player.visible = false
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+	player.global_position = player_spawn.global_position
+	player.scale = Vector2(0.8, 0.8)
+
+	toucan.visible = false
+	toucan.process_mode = Node.PROCESS_MODE_DISABLED
+
+	scene_camera.enabled = true
+	scene_camera.make_current()
+
+	fake_moko.visible = true
+	anim.play("intro")
 
 func end_intro():
 	intro_finished = true
@@ -77,3 +105,38 @@ func end_intro():
 	toucan.process_mode = Node.PROCESS_MODE_INHERIT
 
 	await get_tree().process_frame
+
+# ============================================================================
+#                           VAGUES D'ENEMIES
+# ============================================================================
+
+# --- Vague de serpents ---
+func start_snake_wave():
+	spawn_snake(snake_spawn_1.global_position)
+	spawn_snake(snake_spawn_2.global_position)
+	spawn_snake(snake_spawn_3.global_position)
+
+	await get_tree().process_frame
+
+	update_wave_state()
+
+func spawn_snake(spawn_position):
+	var snake = snake_scene.instantiate()
+	$World/Arena/WavesEnemies.add_child(snake)
+	snake.global_position = spawn_position
+	snake.add_to_group("snake")
+	snake.z_index = 15
+	snake.get_node("Rotator/Sprite2D").scale.x *= -1
+
+func update_wave_state():
+	var snakes = get_tree().get_nodes_in_group("snake")
+	enemies_alive = snakes.size()
+
+func end_snake_wave():
+	start_public_anim()
+	
+	await get_tree().create_timer(0.5).timeout
+	tribune_thrower.throw_snake_wave = true
+
+	await get_tree().create_timer(0.5).timeout
+	stop_public_anim()
