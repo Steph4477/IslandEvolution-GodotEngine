cc000ec6ed3e24467f82c0b0c06832430398ca06
Steph4477 <morelstephane4026@neuf.fr>
2026-05-13 06:11:34 +0200

    feat(tribune): ajout du système de vagues de loot de tribune
    - ajout des vagues de loots serpent et croco
    
    - ajout des bool de déclenchement des vagues
    
    - ajout du pilotage des jets de loots depuis la tribune
    
    - suppression de l'ancienne scène tribune

---
+++ Levels/Lvl3/Lvl_3b/Assets/Tribune/tribune_thrower.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var snake_loots = $WaveSnakeLoots
+@onready var croco_loots = $WaveCrocoLoots
+
+@export var throw_snake_wave = false
+@export var throw_croco_wave = false
+
+
+func _process(_delta):
+
+	if throw_snake_wave:
+
+		throw_snake_wave = false
+
+		for loot in snake_loots.get_children():
+			loot.throw_now()
+
+	if throw_croco_wave:
+
+		throw_croco_wave = false
+
+		for loot in croco_loots.get_children():
+			loot.throw_now()
