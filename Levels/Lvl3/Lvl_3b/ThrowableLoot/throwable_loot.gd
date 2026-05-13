6d5777f77e9a6689fbf469bfa914db9e18e30890
Steph4477 <morelstephane4026@neuf.fr>
2026-05-13 06:09:03 +0200

    feat(throwable_loot): ajout du système de loot physique lançable
    - ajout de ThrowableLoot avec RigidBody2D
    
    - ajout du système de lancement physique des loots
    
    - préparation des loots pour les vagues de l'arène

---
+++ Levels/Lvl3/Lvl_3b/ThrowableLoot/throwable_loot.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var body = $RigidBody2D
+
+@export var loot_scene: PackedScene
+@export var throw_velocity = Vector2(250, -450)
+@export var random_x = 100
+@export var random_rotation = 8
+@export var is_template = false
+
+var loot_spawned = false
+
+
+func _ready():
+	if is_template:
+		visible = false
+		body.freeze = true
+		return
+
+	visible = false
+	body.freeze = true
+
+
+func throw_now():
+	if loot_spawned == false:
+		var loot = loot_scene.instantiate()
+
+		body.add_child(loot)
+
+		loot.position = Vector2.ZERO
+
+		loot_spawned = true
+
+	visible = true
+
+	body.freeze = false
+
+	body.linear_velocity = Vector2(
+		throw_velocity.x + randf_range(-random_x, random_x),
+		throw_velocity.y
+	)
+
+	body.angular_velocity = randf_range(
+		-random_rotation,
+		random_rotation
+	)
