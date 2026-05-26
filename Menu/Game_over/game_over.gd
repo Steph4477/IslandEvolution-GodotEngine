a39f1829c028efea0c44b61e3a7076d2698a1dfc
Steph4477 <morelstephane4026@neuf.fr>
2026-05-26 18:07:09 +0200

    feat(game_over): amélioration de la scène
    - adaptation aux nouveaux gamepads tactiles
    
    - suppression des bordures
    
    - ajout d’une animation d’apparition

--- Menu/Game_over/game_over.gd
+++ Menu/Game_over/game_over.gd
@@ -3,6 +3,7 @@ extends Node2D
 var gs
 
 func _ready ():
+	$sound/GameOver.play()
 	gs = get_node("/root/GameState")
 
 func _on_restart_pressed() -> void:
@@ -11,3 +12,6 @@ func _on_restart_pressed() -> void:
 
 func _on_menu_pressed() -> void:
 	gs.load_level("res://Levels/Lvl0/lvl_0.tscn")
+
+func _on_options_pressed():
+	gs.load_level("res://Interface/Configuration/configuration.tscn")
