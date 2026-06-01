68a54d289b026287606a24a684711eb635ee77ea
Steph4477 <morelstephane4026@neuf.fr>
2026-06-01 14:44:01 +0200

    fix(trap): activation instantanée du piège

--- Levels/Lvl3/Environement/Hidden_Trap/hidden_trap.gd
+++ Levels/Lvl3/Environement/Hidden_Trap/hidden_trap.gd
@@ -3,9 +3,6 @@ extends Node2D
 func _ready():
 	$LeafSprite.visible = true
 	$HoleSprite.visible = false
-	
-	# Le piège est actif au départ
-	$LeafArea/CollisionShape2D.disabled = false
 	$Harrow.visible = false
 
 func _on_area_2d_body_entered(body):
@@ -14,6 +11,3 @@ func _on_area_2d_body_entered(body):
 		$LeafSprite.visible = false
 		$HoleSprite.visible = true
 		$Harrow.visible = true
-		
-		# Désactive la colision des feuilles en differé
-		$LeafArea/CollisionShape2D.set_deferred("disabled", true)
