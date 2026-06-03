b08c1eeb6620d895553f96a0d1b7f9ff33b13c3d
Steph4477 <morelstephane4026@neuf.fr>
2026-06-03 17:21:03 +0200

    feat(animation végetations): ajout d'animations d'effet de vent

--- ItemsDecors/Liana/Liana.gd
+++ ItemsDecors/Liana/Liana.gd
@@ -10,6 +10,12 @@ var expect_exit = false
 
 @onready var pivot = $Pivot
 @onready var collision_shape = $Pivot/Area2D/CollisionShape2D
+@onready var anim = $Pivot/AnimationPlayer
+
+
+func _ready():
+	anim.play("idle")
+
 
 func _process(delta):
 	var target = 0.0
@@ -26,12 +32,15 @@ func _process(delta):
 	else:
 		pivot.rotation_degrees += step * sign(diff)
 
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		attached_player = body
 		body.movement_mod.attach_to_liana(self)
 		swinging = true
 		expect_exit = false
+		anim.stop()
+
 
 func _on_area_2d_body_exited(body):
 	if body == attached_player and expect_exit:
@@ -39,13 +48,18 @@ func _on_area_2d_body_exited(body):
 		angle_direction = 0
 		attached_player = null
 		expect_exit = false
+		anim.play("idle")
+
 
 func on_player_detach():
 	swinging = false
 	angle_direction = 0
 	attached_player = null
+	anim.play("idle")
+
 	if pivot.rotation_degrees < 0.5:
 		pivot.rotation_degrees = 0.0
+
 
 func disable_collision_temporarily(time = 0.3):
 	collision_shape.disabled = true
