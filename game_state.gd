16f67dd3320134aa6237522f0f23cbfdc7c2fe84
Steph4477 <morelstephane4026@neuf.fr>
2026-06-09 22:29:46 +0200

    feat(fire): réutilisation et centrage des flammes
    - recentrage de la flamme dans sa scène
    
    - utilisation de la flamme dans les torches du lvl2
    
    - correction du positionnement des flammes de l'autel de craft

--- game_state.gd
+++ game_state.gd
@@ -181,10 +181,10 @@ func _ready():
 	print("SURVIVOR UNLOCKED : ", survivor_unlocked)
 	print("KING UNLOCKED : ", king_unlocked)
 	
-	#await load_level("res://Levels/IntroCinematic/intro_cinematic.tscn")
+	await load_level("res://Levels/IntroCinematic/intro_cinematic.tscn")
 	#await load_level("res://Levels/Test/test_scene.tscn")
 	#await load_level("res://Levels/Lvl0/lvl_0.tscn")
-	await load_level("res://Levels/Lvl1/lvl_1.tscn")
+	#await load_level("res://Levels/Lvl1/lvl_1.tscn")
 	#await load_level("res://Levels/Lvl2/lvl_2.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2a/lvl_2a.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2b/lvl_2b.tscn")
