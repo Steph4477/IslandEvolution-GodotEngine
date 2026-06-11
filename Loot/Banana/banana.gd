20955fc535c3d14f8fbd3eb560b25f1477c336ac
Steph4477 <morelstephane4026@neuf.fr>
2026-06-12 01:18:57 +0200

    feat(loot): conservation des loots après respawn
    - suivi des bananes collectées
    
    - suivi des noix de coco collectées
    
    - suppression visuelle après mort

--- Loot/Banana/banana.gd
+++ Loot/Banana/banana.gd
@@ -1,20 +1,33 @@
 extends Node2D
 
+@export var loot_id = ""
 @export var banane_value = 5 
 @export var heal_amount = GameBalance.PLAYER_HEAL["banana"]
 
-var game_state
+var gs
 var collected = false
 
 func _ready():
-	game_state = get_node_or_null("/root/GameState")
-	game_state.heal_amount = heal_amount # Initialise me montant de pv par potion
+	gs = get_node_or_null("/root/GameState")
+	gs.heal_amount = heal_amount
+
+	if loot_id == "":
+		loot_id = name
+
+	call_deferred("check_collected")
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func check_collected():
+	if gs.collected_loot_ids.has(loot_id):
+		queue_free()
+
+
+func _on_area_2d_body_entered(body):
 	if collected:
 		return
+
 	if body.is_in_group("Player"):
 		collected = true
 		body.collect_items.collect_banane(banane_value)
+		gs.add_loot_collected(loot_id)
 		queue_free()
--- Loot/Coconut/coconut.gd
+++ Loot/Coconut/coconut.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
-@export var coco_value = 1
-@export var activate_shooting = false
+@export var loot_id = ""
+@export var coco_value = 3
+@export var activate_shooting = true
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+var gs
+
+func _ready():
+	gs = get_node("/root/GameState")
+	if loot_id == "":
+		loot_id = name
+	call_deferred("check_collected")
+
+func check_collected():
+	if gs.collected_loot_ids.has(loot_id):
+		queue_free()
+
+func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
-		body.collect_items.collect_coco(3, true)  
+		body.collect_items.collect_coco(coco_value, activate_shooting)
+		body.game_state.add_loot_collected(loot_id)
 		queue_free()
