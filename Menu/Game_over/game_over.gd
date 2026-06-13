58284eeca7f190bde0acdb9a2f8b05529a09b094
Steph4477 <morelstephane4026@neuf.fr>
2026-06-13 15:44:44 +0200

    feat(game_over): ajout des boutons continuer et retour au menu principal

--- Menu/Game_over/game_over.gd
+++ Menu/Game_over/game_over.gd
@@ -7,11 +7,19 @@ func _ready():
 	gs = get_node("/root/GameState")
 
 func _on_restart_pressed():
+	gs.reset_progression()
 	gs.reset_lives()
-	gs.load_level(gs.current_level_path)
+	gs.reinitialise()
+	gs.reset_session_dialogues()
+	gs.load_level("res://Levels/Lvl1/lvl_1.tscn")
 
 func _on_menu_pressed():
 	gs.load_level("res://Levels/Lvl0/lvl_0.tscn")
 
 func _on_options_pressed():
 	gs.load_level("res://Interface/Configuration/configuration.tscn")
+
+
+func _on_continue_pressed():
+	gs.load_global_progress()
+	await gs.continue_from_unlocked_level()
