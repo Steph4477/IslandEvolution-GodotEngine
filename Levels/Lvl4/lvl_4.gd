c37149effdb394d4bfcf164ae8aeccfd77876d2c
Steph4477 <morelstephane4026@neuf.fr>
2026-06-18 15:38:20 +0200

    feat(lvl4): mise en place du décors aquatique

--- Levels/Lvl4/lvl_4.gd
+++ Levels/Lvl4/lvl_4.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var cam
 var gs
-
+var player
 # --- Réglages du tremblement caméra et dégâts ---
 @export var intensity = 50.0   # Intensité du shake de caméra
 @export var duration = 3.0     # Durée du tremblement
@@ -14,7 +14,10 @@ func _ready():
 	# --- Limite caméra ---
 	await get_tree().process_frame
 	gs = get_node("/root/GameState")
+
 	cam = gs.player.get_node("Camera2D")
-	cam.limit_top = -250000
-	cam.limit_right = 8500
-	cam.limit_bottom = 1400
+	cam.limit_right = 2600
+	cam.limit_bottom = 1500
+
+	player = gs.player
+	player.scale = Vector2(0.7, 0.7)
