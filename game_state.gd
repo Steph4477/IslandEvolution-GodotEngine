5d508c5194f6fff65cfaeea4a7449e306e745082
Steph4477 <morelstephane4026@neuf.fr>
2026-09-09 14:05:00 +0200

    Backup avant prototype 2.5D

--- game_state.gd
+++ game_state.gd
@@ -200,10 +200,10 @@ func _ready():
 	print("KING UNLOCKED : ", king_unlocked)
 	
 	#await load_level("res://Levels/Test/test_scene.tscn")
-	#await load_level("res://Levels/Loader/loader.tscn")
+	await load_level("res://Levels/Loader/loader.tscn")
 	#await load_level("res://Levels/IntroCinematic/intro_cinematic.tscn")
 	#await load_level("res://Levels/Lvl0/lvl_0.tscn")
-	await load_level("res://Levels/Lvl1/lvl_1.tscn")
+	#await load_level("res://Levels/Lvl1/lvl_1.tscn")
 	#await load_level("res://Levels/Lvl2/lvl_2.tscn")
 	#await load_level("res://Levels/Lvl2/lvl_2a/lvl_2a.tscn")
 	#await load_level("res://Levels/Lvl2/Lvl_2b/lvl_2b.tscn")
